f17564fcf3c4c804cb5d867c210b7f516c22ed0c
Kinoko_2k <65443692+kinoko2k@users.noreply.github.com>
2020-09-11 16:52:57 +0900

    tensou.discord.bot
    転送

---
+++ tensou.py
@@ -0,0 +1,17 @@
+import discord
+import asyncio
+from discord.ext import commands
+
+client = discord.Client()  # 接続に使用する
+bot = commands.Bot(command_prefix='$')
+
+@client.event
+async def on_ready(): # botが起動したときに動作する処理
+    print('ログインしました')
+    await client.change_presence(activity=discord.Game(name="Call56NetWorkサーバーログ転送中", type=1))
+
+@bot.command(name="")
+async def rank(ctx):
+    await ctx.send(f"https://cdn.discordapp.com/attachments/725964985670303795/746604189337255936/card.png")
+
+client.run('TOKEN')
