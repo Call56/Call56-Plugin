493f94013a937ff67616264e27f142614ed74793
Kinoko_2k <65443692+kinoko2k@users.noreply.github.com>
2020-09-19 21:55:31 +0900

    tensou2.py
    コマンド追加

--- tensou.py
+++ tensou.py
@@ -10,7 +10,7 @@ async def on_ready(): # botが起動したときに動作する処理
     print('ログインしました')
     await client.change_presence(activity=discord.Game(name="Call56NetWorkサーバーログ転送中", type=1))
 
-@bot.command(name="")
+@bot.command(name="a")
 async def rank(ctx):
     await ctx.send(f"https://cdn.discordapp.com/attachments/725964985670303795/746604189337255936/card.png")
 
