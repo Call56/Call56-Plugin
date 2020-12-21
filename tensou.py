b85921b318ba7d9ab7f1b3a498d10d1940f2df45
Call56 <71284842+Call56@users.noreply.github.com>
2020-12-21 18:11:00 +0900

    Update tensou.py

--- tensou.py
+++ tensou.py
@@ -1,17 +1,15 @@
 import discord
-import asyncio
 from discord.ext import commands
 
-client = discord.Client()  # 接続に使用する
 bot = commands.Bot(command_prefix='$')
 
-@client.event
+@bot.event
 async def on_ready(): # botが起動したときに動作する処理
     print('ログインしました')
     await client.change_presence(activity=discord.Game(name="Call56NetWorkサーバーログ転送中", type=1))
 
-@bot.command(name="a")
-async def rank(ctx):
-    await ctx.send(f"https://cdn.discordapp.com/attachments/725964985670303795/746604189337255936/card.png")
+@bot.command()
+async def rank(rank):
+    await rank.send("https://cdn.discordapp.com/attachments/725964985670303795/746604189337255936/card.png")
 
-client.run('TOKEN')
+bot.run('TOKEN')
